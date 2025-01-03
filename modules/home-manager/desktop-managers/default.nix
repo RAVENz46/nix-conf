dae44f9cd4e3cfe68a5074de2d5e08093d50666e
RAVENz46 <github.shimmy140@passmail.net>
2025-01-03 13:42:34 +0900

    Update

--- modules/home-manager/desktop-managers/default.nix
+++ modules/home-manager/desktop-managers/default.nix
@@ -60,6 +60,7 @@ in
           "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/gtk.css";
         "gtk-4.0/gtk-dark.css".source =
           "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/gtk-dark.css";
+        #"mozc/config1.db".source = ./config1.db;
       };
 
       programs.eww = {
